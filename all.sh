9bf0f36b48eed374713ad9536e8e161fb032db13
Qi Fan <26087110+fanq15@users.noreply.github.com>
2020-08-12 07:16:33 +0800

    Update all.sh
    fix support path

--- all.sh
+++ all.sh
@@ -1,4 +1,4 @@
-rm support_feature.pkl
+rm support_dir/support_feature.pkl
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 fsod_train_net.py --num-gpus 4 \
 	--config-file configs/fsod/R_50_C4_1x.yaml 2>&1 | tee log/fsod_train_log.txt
 
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python3 fsod_train_net.py --num-gpus 4 \
 #	--config-file configs/fsod/R_50_C4_1x.yaml \
 #	--eval-only MODEL.WEIGHTS ./output/fsod/R_50_C4_1x/model_final.pth 2>&1 | tee log/fsod_test_log.txt
 
-rm support_feature.pkl
+rm support_dir/support_feature.pkl
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 fsod_train_net.py --num-gpus 4 \
 	--config-file configs/fsod/finetune_R_50_C4_1x.yaml 2>&1 | tee log/fsod_finetune_train_log.txt
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 fsod_train_net.py --num-gpus 4 \
