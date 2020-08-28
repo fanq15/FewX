bba6b0c40a16ffd47b7ce6e910cc8c07b0425958
Qi Fan <26087110+fanq15@users.noreply.github.com>
2020-08-28 16:42:26 +0800

    Update generate_support_data.sh

--- datasets/generate_support_data.sh
+++ datasets/generate_support_data.sh
@@ -7,7 +7,7 @@ ln -s $DATA_ROOT/val2017 ./
 ln -s $DATA_ROOT/annotations ./
 
 python3 1_split_filter.py ./ 
-python3 2_balance.py ./
+#python3 2_balance.py ./
 python3 3_gen_support_pool.py ./
 python3 4_gen_support_pool_10_shot.py ./
 
